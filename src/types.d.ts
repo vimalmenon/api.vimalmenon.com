8b02c68bb83e6fb1689a83347565a3cbc0fff1fc
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-08-12 13:13:09 +0800

    fix eslint errors

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,19 +1,36 @@
 export type TutorialType = "Tutorial" | "Subject" | "Topic";
+
 export interface IBaseTable {
   uid: string;
   createDate: string;
   updatedDate: string;
 }
+
 export interface ITutorial extends IBaseTable {
   title: string;
+  description: string;
   type: TutorialType;
 }
+
 export interface ISubject extends IBaseTable {
   title: string;
+  description: string;
   type: TutorialType;
+  tutorialId: string;
 }
+
 export interface ITopic extends IBaseTable {
   title: string;
+  description: string;
   type: TutorialType;
-  content: string;
+  content: ITopicContent;
+  subjectId: string;
+}
+
+export interface ITopicContent {
+  code?: string;
+  options?: string[];
+  description: string[];
+  note?: string;
+  youtube?: string;
 }
